2982a3d5cf623101d28f3224de552737911b66a6
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2020-09-18 03:17:22 +0530

    Adds sysinfo to get general statistics

--- app/src/main/cpp/perf.cpp
+++ app/src/main/cpp/perf.cpp
@@ -3,10 +3,14 @@
 //
 #include <jni.h>
 #include "jni_interface.h"
+#include <sys/sysinfo.h>
 #include "android_debug.h"
 
 extern "C" JNIEXPORT jstring JNICALL Java_com_developers_perfmon_MainActivity_getSysInfo(JNIEnv *env, jobject) {
     LOGD("Hello from C++ world");
+    struct sysinfo si{};
+    sysinfo (&si);
+    LOGD("Hello from C++ world with %d", si.procs);
     return (*env).NewStringUTF("Hello from C");
 }
 
